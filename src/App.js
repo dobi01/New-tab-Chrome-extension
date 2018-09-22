9705e40048e5b726ced3b6c2bb18fc832db20779
Dobi Okrasa <dobi.okrasa@gmail.com>
2018-09-22 19:52:22 +0200

    Add initial view

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.load = () => {
+      this.state.name = localStorage.getItem('name');
+    }
+    if (localStorage.length) this.load();
+  }
+
+  handleChange(e) {
+    if (e.which == 13 || e.keyCode == 13) {
+      let name = e.target.value;
+      localStorage.setItem('name', name);
+      this.setState({name});
+    }
+  }
+
   render() {
+    const welcome = <h2>Welcome, {this.state.name}</h2>,
+          enterName = <input type="text" placeholder="Enter your name" onKeyDown={this.handleChange}/>;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <h1 className="App-title">Room 156</h1>
+          <div class="o">
+            <p>enter</p>
+          </div>
+          {/* {this.state.name ? welcome : enterName} */}
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
